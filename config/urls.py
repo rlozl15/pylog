102f15b6fb0ba4a20faf3c04f2e5689f7cadf282
rlozl15 <eunjikim.dev@gmail.com>
2024-10-29 04:49:39 +0900

    feat: add user upload static file setting

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 from config.views import index
 from blog.views import post_list
 
@@ -24,3 +27,8 @@ urlpatterns = [
     path('', index),
     path('posts/', post_list),
 ]
+
+urlpatterns += static(
+    prefix = settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT,
+)
