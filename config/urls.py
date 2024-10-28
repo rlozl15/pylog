5e971dd882f555174a2afdb0e4de9fe1104224cd
rlozl15 <eunjikim.dev@gmail.com>
2024-10-28 22:08:30 +0900

    feat: add blog listing page and style setting

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from config.views import index
+from blog.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('posts/', post_list),
 ]
