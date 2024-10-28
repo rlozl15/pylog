7978656c5fbe3a2341b74c80d8e3ab543892dee5
rlozl15 <eunjikim.dev@gmail.com>
2024-10-29 07:57:54 +0900

    feat: add detail page

--- config/urls.py
+++ config/urls.py
@@ -20,12 +20,13 @@ from django.contrib import admin
 from django.urls import path
 
 from config.views import index
-from blog.views import post_list
+from blog.views import post_list, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('posts/', post_list),
+    path('posts/<int:post_id>/', post_detail),
 ]
 
 urlpatterns += static(
